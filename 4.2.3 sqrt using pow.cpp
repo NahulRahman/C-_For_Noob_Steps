81881e6e5292dd82023dab8c4b006bdd59e29933
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-13 01:58:27 +0600

    Add files via upload

---
+++ 4.2.3 sqrt using pow.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    double a,b,c;
+    cin>>a>>b>>c;
+    cout<<pow(a,1.0/2.0)<<endl<<pow(b,1.0/2.0)<<endl<<pow(c,1.0/2.0);
+    return 0;
+}
+
