532501518d66faeb23361881b5139c849ad99035
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-12 20:02:07 +0600

    Add files via upload

---
+++ 2. increment decrement in arithmetic.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+//increment and decrement operators are included
+//pre increment ++X  first increment then print
+//post increment X++ first print then increment
+   cout<<"A++ = "<<a++<<endl;
+    cout<<"++A = "<<++a<<endl;
+    cout<<"A-- = "<<a--<<endl;
+    cout<<"--A = "<<--a<<endl;
+    cout<<"B++ = "<<b++<<endl;
+    cout<<"++B = "<<++b<<endl;
+    cout<<"B-- = "<<b--<<endl;
+    cout<<"--B = "<<--b<<endl;
+    return 0;
+}
