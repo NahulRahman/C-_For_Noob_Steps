8039edf54c3036a8cc7b2455d338ce1d48eba75c
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-13 01:55:35 +0600

    Add files via upload

---
+++ 4.2.4 sqrt using pow & setprecision.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+int main(){
+    double a,b,c;
+    cin>>a>>b>>c;
+    cout<<fixed<<setprecision(5)<<pow(a,1.0/2.0)<<endl<<pow(b,1.0/2.0)<<endl<<pow(c,1.0/2.0);
+    return 0;
+}
+
+
