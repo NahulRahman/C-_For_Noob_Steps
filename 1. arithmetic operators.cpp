ceab5cdd379f65218cc503723272bdea6e1e4ec2
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-12 20:01:40 +0600

    Add files via upload

---
+++ 1. arithmetic operators.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<"A   = "<<a<<endl;
+    cout<<"B   = "<<b<<endl;
+    cout<<"A+B = "<<a+b<<endl;
+    cout<<"A-B = "<<a-b<<endl;
+    cout<<"A*B = "<<a*b<<endl;
+    cout<<"A/B = "<<a/b<<endl;
+    cout<<"A%B = "<<a%b<<endl;
+    //increment and decrement operators are included
+    cout<<"A++ = "<<a++<<endl;
+    cout<<"++A = "<<++a<<endl;
+    cout<<"A-- = "<<a--<<endl;
+    cout<<"--A = "<<--a<<endl;
+    cout<<"B++ = "<<b++<<endl;
+    cout<<"++B = "<<++b<<endl;
+    cout<<"B-- = "<<b--<<endl;
+    cout<<"--B = "<<--b<<endl;
+    return 0;
+}
