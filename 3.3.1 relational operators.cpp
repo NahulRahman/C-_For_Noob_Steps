dde4984b697e12808836769ee756c1e16be38943
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-12 20:45:22 +0600

    Add files via upload

---
+++ 3.3.1 relational operators.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b; // let a=1, b=2
+    cin>>a>>b;
+    cout<<(a==b)<<endl; //1==2 false  0
+    cout<<(a!=b)<<endl;// 1!=2 true   1
+    cout<<(a>b)<<endl; // 1>2 false   0
+    cout<<(a<b)<<endl; // 1<2  true   1
+    cout<<(a<=b)<<endl;// 1<=2 true   1
+    cout<<(a>=b)<<endl;// 1>=2 false  0
+    return 0;
+
+}
+
