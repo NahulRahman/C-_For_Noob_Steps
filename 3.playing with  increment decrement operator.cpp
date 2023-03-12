695dbc3003f5a8ac6bf485ef998df4c6339c2a2c
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-12 20:03:44 +0600

    Add files via upload

---
+++ 3.playing with  increment decrement operator.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<"A   = "<<a<<endl;
+    cout<<"B   = "<<b<<endl;
+    cout<<"B   = "<<b<<endl;
+    cout<<"A++ = "<<a++<<endl;
+    cout<<"A   = "<<a<<endl;
+    cout<<"++A = "<<++a<<endl;
+    cout<<"A   = "<<a<<endl;
+    cout<<"A-- = "<<a--<<endl;
+    cout<<"A   = "<<a<<endl;
+    cout<<"--A = "<<--a<<endl;
+    cout<<"A   = "<<a<<endl;
+    cout<<"B++ = "<<b++<<endl;
+    cout<<"B   = "<<b<<endl;
+    cout<<"++B = "<<++b<<endl;
+    cout<<"B   = "<<b<<endl;
+    cout<<"B-- = "<<b--<<endl;
+    cout<<"B   = "<<b<<endl;
+    cout<<"--B = "<<--b<<endl;
+    cout<<"B   = "<<b<<endl;
+    cout<<"A   = "<<a<<endl;
+    cout<<"B   = "<<b<<endl;
+    return 0;
+}
