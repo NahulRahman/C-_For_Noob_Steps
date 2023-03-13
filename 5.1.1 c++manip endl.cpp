53c9b9cf6861da18787bd0fee2c78d01dba56245
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-13 16:43:47 +0600

    Add files via upload

---
+++ 5.1.1 c++manip endl.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    int a=1,b=10,c=100,d=1000;
+    cout<<a<<endl<<b<<endl<<c<<endl<<d<<endl;
+    return 0;
+}
+/*
+o/p
+1
+10
+100
+1000
+*/
