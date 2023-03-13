bfb97b5936ebfcbd089adbe67b826fdf7d11f161
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-13 16:42:05 +0600

    Add files via upload

---
+++ 5.2.3 c++ manip setw.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    int a=1,ab=10,abc=100,abcd=1000;
+    cout<<"a   ="<<setw(4)<<a<<endl;
+    cout<<"ab  ="<<setw(4)<<ab<<endl;
+    cout<<"abc ="<<setw(4)<<abc<<endl;
+    cout<<"abcd="<<setw(4)<<abcd<<endl;
+    return 0;
+}
+/*
+o/p;
+a   =   1
+ab  =  10
+abc = 100
+abcd=1000
+*/
