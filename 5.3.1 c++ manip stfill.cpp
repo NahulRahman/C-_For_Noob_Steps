901f8c47509eebae659875dd745768f1ee9477d2
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-13 16:42:43 +0600

    Add files via upload

---
+++ 5.3.1 c++ manip stfill.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    int a=1,b=10,c=100,d=1000;
+    cout<<setfill('*')<<setw(4)<<a<<endl<<setw(4)<<b<<endl<<setw(4)<<c<<endl<<setw(4)<<d<<endl;
+    return 0;
+}
+/*
+o/p:
+***1
+**10
+*100
+1000
+*/
+
