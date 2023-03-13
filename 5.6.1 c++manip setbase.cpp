6e60b5e280934aa6e96381aff820b187a26660c4
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-13 16:39:01 +0600

    Add files via upload

---
+++ 5.6.1 c++manip setbase.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    int a;
+    cout<<"enter a decimal number:"<<endl;
+    cin>>a;
+    cout<<"Decimal=>Octal:="<<setbase(8)<<a<<endl;
+    cout<<"Decimal=>Octal:="<<setbase(8)<<a<<endl;
+    cout<<"Decimal=>Hex:="<<setbase(8)<<a<<endl;
+    cout<<"Decimal=>Hex:="<<setbase(8)<<a<<endl;
+    return 0;
+}
+
