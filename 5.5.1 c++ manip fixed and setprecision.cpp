9784b6e45ed16904ad2ff8ba9d63251157aeb5ac
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-13 16:39:58 +0600

    Add files via upload

---
+++ 5.5.1 c++ manip fixed and setprecision.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    double a;
+    cin>>a;
+    cout<<fixed<<setprecision(3)<<a<<endl;
+    cout<<a<<endl;
+    cout<<a<<endl;
+    return 0;
+}
+/*
+i/p:
+4.765432
+
+o/p:
+4.765
+4.765
+4.765
+*/
