49ba407bc1fdd73efbdb10f53c2ef7d6e10b057b
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-13 16:40:25 +0600

    Add files via upload

---
+++ 3.4.2 c++manip setprecision.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main(){
+    double a;
+    cin>>a;
+    cout<<setprecision(1)<<a<<endl;
+    cout<<setprecision(2)<<a<<endl;
+    cout<<setprecision(3)<<a<<endl;
+    cout<<setprecision(4)<<a<<endl;
+    cout<<setprecision(5)<<a<<endl;
+    cout<<a<<endl;
+    cout<<a<<endl;
+    return 0;
+}
+/*
+i/p
+3.876543
+
+o/p
+4
+3.9
+3.88
+3.877
+3.8765
+3.8765
+3.8765
+
+*/
