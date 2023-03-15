9e9d66e9a8d5e092aeed505116c3d1d8e38f861e
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-16 00:03:36 +0600

    Add files via upload

---
+++ c3loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int i,j,n,k=1;
+    cin>>n;
+    for(i=0;i<n;i++){
+        for(j=0;j<i;j++){
+            cout<<k%2;
+            k++;
+        }cout<<endl;
+    }
+
+    return 0;
+}
