90c1839f4cbdb88ca6a7e83384570f256fb6645f
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-16 00:06:52 +0600

    Add files via upload

---
+++ c2loop.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    // Write C++ code here
+    int i,j,n,k=1;
+    cin>>n;
+    for(i=0;i<n;i++){
+            for(j=0;j<=n-i;j++) cout<<" ";
+        for(j=0;j<i;j++){
+           (k%2!=0)?cout<<"A":cout<<"B";
+            k++;
+        }cout<<endl;
+    }
+
+    return 0;
+}
