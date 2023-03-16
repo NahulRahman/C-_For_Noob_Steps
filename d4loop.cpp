2175f4e8497adb2ea01b9b92f98b8722daa96b43
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-16 19:22:33 +0600

    Add files via upload

---
+++ d4loop.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i, j, rows;
+    int stars, spaces;
+    cin>>rows;
+    stars = 1;
+    spaces = rows - 1;
+    for(i=1; i<rows*2; i++){
+        for(j=1; j<=spaces; j++) cout<<" ";
+        for(j=1; j<stars*2; j++) cout<<"*"<<"      ";
+        cout<<endl;
+        if(i<rows){
+            spaces--;
+            stars++;
+        }else{
+            spaces++;
+            stars--;
+        }
+    }return 0;
+}
