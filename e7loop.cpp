8283d98914bc4a22bf0c68bb85e5182c6f061196
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-17 00:44:38 +0600

    Add files via upload

---
+++ e7loop.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,j;
+    cin>>n;
+
+    for(i=1;i<=n;i++){
+            for(j=1;j<=n-i;j++) cout<<" ";
+        for(j=1;j<=(2*i-1);j++){
+            if(j%2!=0) cout<<"A    ";
+            else cout<<"B    ";
+        }cout<<endl;
+    }
+
+    for(i=1;i<=n;i++){
+            for(j=1;j<i;j++) cout<<" ";
+        for(j=1;j<=(2*n-(2*i-1));j++){
+            if(j%2!=0) cout<<"A    ";
+            else cout<<"B    ";
+        }cout<<endl;
+    }
+    return 0;
+}
