ed00d12f20daf4a215afab748461ac911951f15d
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-19 01:21:36 +0600

    Add files via upload

---
+++ f1loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int i,j,n;
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            if(j%2!=0) cout<<"A";
+                else cout<<"B";
+        }cout<<endl;
+    }return 0;
+}
