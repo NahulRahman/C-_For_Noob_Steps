792db5aafe3d0f234ed468cd8a075cf29075da74
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-24 00:24:50 +0600

    Add files via upload

---
+++ h7loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n,k=1;
+    cin>>n;
+    for (i=1;i<=n;i++){
+        for(j=1;j<=n-i;j++){ cout<<" ";
+        }for(j=1;j<=i;j++){
+            (k%2==0)?cout<<"A":cout<<"B";
+            k++;
+        }cout<<endl;
+    }return 0;
+}
