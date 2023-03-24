a610f019f62762ad90c21efa35b01b1d35d16b3c
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-24 16:19:30 +0600

    Add files via upload

---
+++ m9loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n;
+    double k=1;
+    cin>>n;
+for(i=1;i<=n;i++){
+        for(j=1;j<i;j++) cout<<" ";
+        for(j=i;j<=n;j++){
+                cout<<k/4<<"        ";
+                k++;
+        }cout<<endl;
+     }return 0;
+     }
+
