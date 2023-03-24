51ba0b4ef1d9184a1b3ebf7ed3cadb647d8cbac5
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-24 16:28:43 +0600

    Add files via upload

---
+++ l8loop.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n;
+    cin>>n;
+
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-i;j++) cout<<" ";
+        for(j=1;j<=i;j++) cout<<j%2<<"       ";
+        cout<<endl;
+    }
+
+     for(i=1;i<=n;i++){
+        for(j=1;j<i;j++) cout<<" ";
+        for(j=i;j<=n;j++) cout<<j%2<<"       ";
+        cout<<endl;
+     }
+    return 0;
+}
