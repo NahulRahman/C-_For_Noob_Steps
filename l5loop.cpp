cf4ae8627df420ec37bdccd0e18633a4421b373e
MD. NAHUL RAHMAN <123864673+NahulRahman@users.noreply.github.com>
2023-03-24 16:38:06 +0600

    Add files via upload

---
+++ l5loop.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i,j,n,k=1;
+    cin>>n;
+
+    for(i=1;i<=n;i++){
+        for(j=1;j<=n-i;j++) cout<<" ";
+        for(j=1;j<=i;j++) {
+            (k%2==0)?cout<<"A      ":cout<<"B      ";
+            k++;
+        }
+        cout<<endl;
+    }
+
+     for(i=1;i<=n;i++){
+        for(j=1;j<i;j++) cout<<" ";
+        for(j=i;j<=n;j++){
+            (k%2!=0)?cout<<"A      ":cout<<"B      ";
+            k++;
+        }
+        cout<<endl;
+     }
+    return 0;
+}
